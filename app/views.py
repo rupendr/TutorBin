4bb801bc7814a81f3b71deafb9edd79238a4c9be
rupendr <rupendrarathod@gmail.com>
2020-07-03 23:44:07 +0530

    html change

--- app/views.py
+++ app/views.py
@@ -1,11 +1,9 @@
-from django.shortcuts import render, HttpResponse
+from django.shortcuts import render
 from .models import question
-from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 
 
 # Create your views here.
 def index(request):
     questions = question.objects.all()
-
     return render(request, 'question_list.html',
                   {'questions': questions, 'message': 'file format not supported, click here to download'})
